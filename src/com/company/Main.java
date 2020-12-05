f8fd02b5f2af3a961b1fbc7cc0d0051ebefb886e
SeanDunlop <30559729+SeanDunlop@users.noreply.github.com>
2020-12-05 15:36:31 -0500

    print func

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
 	// write your code here
+
+        print("Hello World");
+
+
+    }
+
+    public static void print(Object o)
+    {
+        System.out.println(o);
     }
 }
